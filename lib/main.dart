dd035bb2c73d671871e44db2b12adc3d27c8cc21
tzupingchenwork <andychentzupingwork@gmail.com>
2024-01-26 09:00:18 +0800

    Add paginated Data Table example

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,8 @@ class MyApp extends StatelessWidget {
       home: const MyHomePage(title: 'Flutter data table'),
       routes: {
         '/dataTable2': (context) => const DataTableExamplePage(),
+        '/paginatedDataTable2': (context) =>
+            const PaginatedDataTableExamplePage(),
       },
     );
   }
@@ -43,11 +45,22 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pushNamed(context, '/dataTable2');
-          },
-          child: const Text('Go to Data Table 2 example'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/dataTable2');
+              },
+              child: const Text('Go to Data Table 2 example'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/paginatedDataTable2');
+              },
+              child: const Text('Go to paginated Data Table2 example'),
+            ),
+          ],
         ),
       ),
     );
@@ -117,3 +130,122 @@ const String _jsonResponse = '''
   {"columnA": "Row 3A", "columnB": "Row 3B", "columnC": "Row 3C"}
 ]
 ''';
+
+class PaginatedDataTableExamplePage extends StatelessWidget {
+  const PaginatedDataTableExamplePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Paginated Data Table Example'),
+      ),
+      body: const Card(
+        color: Color.fromARGB(255, 10, 139, 204),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: PaginatedDataTableExample(),
+        ),
+      ),
+    );
+  }
+}
+
+class PaginatedDataTableExample extends StatefulWidget {
+  const PaginatedDataTableExample({super.key});
+
+  @override
+  State<PaginatedDataTableExample> createState() =>
+      _PaginatedDataTableExampleState();
+}
+
+class _PaginatedDataTableExampleState extends State<PaginatedDataTableExample> {
+  late final MyDataTableSource _dataSource;
+
+  @override
+  void initState() {
+    super.initState();
+    List<Map<String, dynamic>> jsonData =
+        List<Map<String, dynamic>>.from(jsonDecode(_jsonResponse) as List);
+    _dataSource = MyDataTableSource(jsonData);
+  }
+
+  int _sortColumnIndex = 0;
+  bool _sortAscending = true;
+
+  void _sort<T>(Comparable<T> Function(Map<String, dynamic> d) getField,
+      int columnIndex, bool ascending) {
+    _dataSource.sort(getField, ascending);
+    setState(() {
+      _sortColumnIndex = columnIndex;
+      _sortAscending = ascending;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PaginatedDataTable2(
+      columnSpacing: 12,
+      horizontalMargin: 12,
+      minWidth: 600,
+      sortColumnIndex: _sortColumnIndex,
+      sortAscending: _sortAscending,
+      columns: [
+        DataColumn2(
+            label: const Text('Column A'),
+            size: ColumnSize.L,
+            onSort: (columnIndex, ascending) =>
+                _sort((d) => d['columnA'], columnIndex, ascending)),
+        DataColumn2(
+            label: const Text('Column B'),
+            size: ColumnSize.L,
+            onSort: (columnIndex, ascending) =>
+                _sort((d) => d['columnB'], columnIndex, ascending)),
+        DataColumn2(
+            label: const Text('Column C'),
+            size: ColumnSize.L,
+            onSort: (columnIndex, ascending) =>
+                _sort((d) => d['columnC'], columnIndex, ascending)),
+      ],
+      rowsPerPage: 2,
+      source: _dataSource,
+    );
+  }
+}
+
+class MyDataTableSource extends DataTableSource {
+  MyDataTableSource(this._rows);
+
+  final List<Map<String, dynamic>> _rows;
+
+  void sort<T>(
+      Comparable<T> Function(Map<String, dynamic> d) getField, bool ascending) {
+    _rows.sort((a, b) {
+      final aValue = getField(a);
+      final bValue = getField(b);
+      return ascending
+          ? Comparable.compare(aValue, bValue)
+          : Comparable.compare(bValue, aValue);
+    });
+    notifyListeners();
+  }
+
+  @override
+  DataRow getRow(int index) {
+    if (index >= _rows.length) return const DataRow(cells: []);
+    return DataRow(cells: [
+      DataCell(Text(_rows[index]['columnA'].toString())),
+      DataCell(Text(_rows[index]['columnB'].toString())),
+      DataCell(Text(_rows[index]['columnC'].toString())),
+    ]);
+  }
+
+  @override
+  bool get isRowCountApproximate => false;
+
+  @override
+  int get rowCount => _rows.length;
+
+  @override
+  int get selectedRowCount => 0;
+}
